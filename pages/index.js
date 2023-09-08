8e22f1fb063e2d3f01b96b6932db94b9044ff0de
Michael <limichael909@gmail.com>
2023-09-08 14:34:53 -0400

    added key

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { useState } from "react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from '@chakra-ui/react'
-import { Box, useDisclosure, useColorMode, Link, useToast, IconButton, useColorModeValue, Tag, Input, Text, SimpleGrid, Button, Center, Img, VStack } from '@chakra-ui/react';
+import { Box, useDisclosure, useColorMode, Link, IconButton, useColorModeValue, Tag, Input, Text, SimpleGrid, Button, Center, Img, VStack } from '@chakra-ui/react';
 
 //Retrieves the initial 1010 pokemon from PokeApi in the server side before the page content is loaded.
 export async function getServerSideProps(context){
@@ -20,9 +20,9 @@ export async function getServerSideProps(context){
   }
 }
 
-function PokemonDiv( {modalBgColor, index, textColor, item}){
+function PokemonDiv( {modalBgColor, index, textColor, item, populateInfo}){
   return(
-    <Box _hover={{cursor: "pointer"}} pos={"relative"} p={5} textAlign="center" key={index} onClick={() => populateInfo(item.name)} border={`2px dashed ${modalBgColor}`} borderRadius={5}>
+    <Box _hover={{cursor: "pointer"}} pos={"relative"} p={5} textAlign="center" onClick={() => populateInfo(item.name)} border={`2px dashed ${modalBgColor}`} borderRadius={5}>
       <Center>
         <Img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}/>
       </Center>
@@ -40,7 +40,6 @@ export default function Home( {pokemonData} ) {
   const modalBgColor = useColorModeValue("#739c39", "#2b2b2b");
   const compColor = useColorModeValue("#b4de73", "#3b3b3b");
   const buttonColor = useColorModeValue("#188f31", "#bcbcbc");
-  const toast = useToast()
 
   const [moves, setMoves] = useState([])
   const [count, setCount] = useState(50)
@@ -81,34 +80,6 @@ export default function Home( {pokemonData} ) {
     })
   }
 
-  //Action performed when a pokemon is searched for in the search bar.
-  function searchPokemon(){
-
-    //Idea: Looks to find the first Pokemon that matches the name exactly in the search bar.
-    //If the name cannot be matched exactly, then we find the first pokemon that contains the name, but does not exactly match it.
-    const toFind=searchText.toLowerCase();
-    let foundName = pokemonData.find((pokemon) => pokemon["name"]===toFind);
-    
-    if (foundName==undefined){
-      foundName = pokemonData.find((pokemon) => pokemon["name"].includes(toFind));
-    }
-    
-    //Informs the client that the Pokemon does not exist.
-    if (foundName==undefined){
-      toast({
-        title: 'Invalid Pokemon.',
-        description: "We did not find this Pokemon in our dataset! Can you guess what Pokemon this is?: 🧲👁️🧲",
-        status: 'error',
-        duration: 5000,
-        position: "top-left",
-        isClosable: true,
-      })
-      return;
-    }
-
-    populateInfo(foundName['name']);
-  }
-
   //Loads more Pokemon onto the main screen
   async function loadMore(){
     setCount(count+40)
@@ -139,9 +110,9 @@ export default function Home( {pokemonData} ) {
         <SimpleGrid minChildWidth='130px' spacing='33px' px={"10%"} my={"3%"}>
           {searchText ? (pokemonData.map((item, index) => (
             (item.name.includes(searchText) && 
-            <PokemonDiv modalBgColor={modalBgColor} index={index} textColor={textColor} item={item}/> 
+            <PokemonDiv populateInfo={populateInfo} modalBgColor={modalBgColor} key={index} index={index} textColor={textColor} item={item}/> 
             )))) : (pokemonData && pokemonData.slice(0, count).map((item, index) => (
-          <PokemonDiv modalBgColor={modalBgColor} index={index} textColor={textColor} item={item}/> 
+          <PokemonDiv populateInfo={populateInfo} modalBgColor={modalBgColor} key={index} index={index} textColor={textColor} item={item}/> 
         )))}
 
         </SimpleGrid>

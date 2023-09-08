3d4fafbd2aadca9a4f3747c0117e08347dea7d2c
Michael <limichael909@gmail.com>
2023-09-07 22:06:22 -0400

    first working website

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { useState } from "react";
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-//linear-gradient(to right, #362039, #36213e, #352343, #332549, #30274e)
+
 const theme = extendTheme({
   styles: {
     global: {
@@ -12,22 +9,15 @@ const theme = extendTheme({
         bg: 'white',
       },
     },
-  },
-  fonts: {
-    heading: `'Open Sans', sans-serif`,
-    body: `'Open Sans', sans-serif`,
   }
 })
 
 function MyApp({ Component, pageProps }) {
-
-
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} /> 
     </ChakraProvider>
   )
-  
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,17 @@
 import Head from 'next/head';
-import Image from 'next/image'
-import styles from '../styles/Home.module.css';
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from '@chakra-ui/react'
+import { Box, useDisclosure, useColorMode, Link, useToast, IconButton, InputGroup, InputRightElement, useColorModeValue, Tag, Input, Text, SimpleGrid, Button, Center, Img, VStack } from '@chakra-ui/react';
 
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-} from '@chakra-ui/react'
-import { Box, useDisclosure, HStack, InputGroup, InputRightElement, useColorModeValue, Tag, Input, Text, SimpleGrid, Button, Flex, Center, Stack, Img, VStack } from '@chakra-ui/react';
-
+//Retrieves the initial 1010 pokemon from PokeApi in the server side before the page content is loaded.
 export async function getServerSideProps(context){
 
+  //Gathers the names of the 1010 Pokemon existing in PokeApi
   let pokemonData=[]
   await fetch('https://pokeapi.co/api/v2/pokemon?limit=1010&offset=0').then(res => res.json()).then(data => {
     pokemonData=data
   })
-
 
   return{
     props:{
@@ -29,40 +19,49 @@ export async function getServerSideProps(context){
     }
   }
 }
+
 export default function Home( {pokemonData} ) {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { colorMode, toggleColorMode } = useColorMode();
+  const textColor = useColorModeValue("#f8f8f8", "#111111");
+  const bodyBgColor = useColorModeValue("#575757", "#bdde7b");
+  const modalBgColor = useColorModeValue("#2b2b2b", "#739c39");
+  const compColor = useColorModeValue("#3b3b3b", "#b4de73");
+  const buttonColor = useColorModeValue("#bcbcbc", "#188f31");
+  const toast = useToast()
 
   const [moves, setMoves] = useState([])
   const [count, setCount] = useState(50)
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
   const [types, setTypes] = useState([])
-  const [species, setSpecies] = useState("")
-  const [desc, setDesc] = useState("")
   const [weight, setWeight] = useState("")
   const [height, setHeight] = useState("")
   const [number, setNumber] = useState("")  
   const [searchText, setSearch] = useState("")
 
+  //Populates the Modal with the pokemon info whenever a new pokemon is selected
   async function populateInfo(pokeName){
 
+    //Retrieves the data for a specific pokemon such as its weight, height, and attack info
     await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then(res => res.json()).then(data => {
-      console.log(data);
       
       const pokeTypes=[];
       const pokeMoves=[];
+
+      //Adds the type of the pokemon
       data.types.forEach((val) => {
         pokeTypes.push(val.type.name)
       })
 
+      //Finds the first four moves in the pokemon's moveset
       for (let i=0; i<4; i++){
         pokeMoves.push(data.moves[i].move.name.replace("-", " ").toUpperCase())
       }
-      console.log(pokeMoves);
+
+      //Setting the modal data
       setMoves(pokeMoves);
       setTypes(pokeTypes);
       setName(pokeName);
-      //setSpecies(pokeData.species);
-      //setDesc(pokeData.description);
       setWeight(data.weight);
       setHeight(data.height);
       setNumber(data.id);
@@ -70,7 +69,11 @@ export default function Home( {pokemonData} ) {
     })
   }
 
+  //Action performed when a pokemon is searched for in the search bar.
   function searchPokemon(){
+
+    //Idea: Looks to find the first Pokemon that matches the name exactly in the search bar.
+    //If the name cannot be matched exactly, then we find the first pokemon that contains the name, but does not exactly match it.
     const toFind=searchText.toLowerCase();
     let foundName = pokemonData.find((pokemon) => pokemon["name"]===toFind);
     
@@ -78,79 +81,86 @@ export default function Home( {pokemonData} ) {
       foundName = pokemonData.find((pokemon) => pokemon["name"].includes(toFind));
     }
     
-    //Show that pokemon is not found
+    //Informs the client that the Pokemon does not exist.
     if (foundName==undefined){
+      toast({
+        title: 'Invalid Pokemon.',
+        description: "We did not find this Pokemon in our dataset! Can you guess what Pokemon this is?: 🧲👁️🧲",
+        status: 'error',
+        duration: 5000,
+        position: "top-left",
+        isClosable: true,
+      })
       return;
     }
 
     populateInfo(foundName['name']);
   }
 
-  //A function that loads more pokemon
+  //Loads more Pokemon onto the main screen
   async function loadMore(){
-
     setCount(count+40)
   }
 
   return (
     
     <>
-
       <Head>
-        <title>PokeArt</title>
+        <title>PokeSearch</title>
       </Head>
-
-      <Box bgColor={"#bdde7b"} h={"100vh"} overflowY={'scroll'} pb={20}>
-        <Center mt={20}>
+      
+      <Box bgColor={bodyBgColor} h={"100vh"} overflowY={'scroll'} pb={20} pos={"relative"}>
+        <IconButton pos={"absolute"} top={10} right={10} bgColor={buttonColor} onClick={toggleColorMode} aria-label='Change Color Scheme' icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />} />
+        <Center mt={20} textAlign={"center"}>
           <VStack>
-            <Text fontSize={40} fontWeight={"bold"} color={"#0e0e0e"}>Search for a pokemon!</Text>
+            <Text fontSize={50} fontWeight={"bold"} color={textColor}>Search for a pokemon!</Text>
             
-            <InputGroup size='md'>
+            <InputGroup size='md' maxW={"80%"}>
               <Input
                 value={searchText} onChange={(e)=>setSearch(e.target.value)}
                 type={'text'}
-                backgroundColor={"white"} borderColor={"black"} borderWidth={4} color={"#0e0e0e"} focusBorderColor='black' placeholder="Search for a Pokemon by their name" 
+                backgroundColor={"white"} borderColor={"black"} borderWidth={4} color={"black"} focusBorderColor='black' placeholder="Search for a Pokemon by their name" 
               />
               <InputRightElement width='4.5rem' mr={2}>
-                <Button h='1.75rem' size='sm' onClick={searchPokemon} colorScheme='green'>Search</Button>
+                <Button h='1.75rem' size='sm' onClick={searchPokemon} colorScheme='green' color={textColor}>Search</Button>
               </InputRightElement>
             </InputGroup>
-            
           </VStack>
-          
-          
         </Center>
+
+        {/* Displays basic info for each Pokemon: A picture, the name, and number */}
         <SimpleGrid minChildWidth='130px' spacing='33px' px={"10%"} my={"3%"}>
   
           {pokemonData && pokemonData.slice(0, count).map((item, index) => (
   
-              <Box pos={"relative"} p={5} textAlign="center" key={index} onClick={() => populateInfo(item.name)} border={"2px dashed #739c39"} borderRadius={5}>
+              <Box _hover={{cursor: "pointer"}} pos={"relative"} p={5} textAlign="center" key={index} onClick={() => populateInfo(item.name)} border={`2px dashed ${modalBgColor}`} borderRadius={5}>
                 <Center>
                   <Img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}/>
                 </Center>
-                <Text fontSize={"14px"} fontWeight={"bold"} color={"black"}>{item.name.toUpperCase()}</Text>
-        
-                <Text color={"#111111"} pos={"absolute"} fontSize={"1.7ch"} top={2} left={3}>{index+1}.</Text>
+                <Text fontSize={"16px"} color={textColor}>{item.name.toUpperCase()}</Text>
+                <Text color={textColor} pos={"absolute"} fontSize={"2ch"} top={2} left={3}>{index+1}.</Text>
            </Box>
               
             )
           )}
         </SimpleGrid>
-        <Center>
-          <Button color={"white"} bgColor={"#186a31"} _hover={{bg: "#31944a"}} onClick={loadMore}>Load More!</Button>
+
+        <Center py={5}>
+          <Button color={colorMode==="light"?"black":"white"} bgColor={buttonColor} onClick={loadMore}>Load More!</Button>
         </Center>
+        <Box>
+          <Text fontSize={30} position={'absolute'} right={20} color={textColor}>Made by <Link href="https://www.itsmichael.dev/" target="_blank" color={colorMode==="light"?"red.400":"teal.500"}>Michael.</Link></Text>
+        </Box>
       </Box>
       
-
+      {/* Modal includes main information about each Pokemon. We gather this data from https://pokeapi.co/*/}
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent bgColor={"#739c39"}>
-          <ModalHeader color={"black"}>{number}. {name.toUpperCase()}
-          
+        <ModalContent bgColor={modalBgColor}>
+          <ModalHeader color={textColor}>{number}. {name.toUpperCase()}
                 {types.map((item, index) => (
-                  <Tag bgColor={"#b4de73"} color={"black"} mt={1} key={index} ml={2}>{item}</Tag>
+                  <Tag bgColor={compColor} color={textColor} mt={1} key={index} ml={2}>{item}</Tag>
                 ))}
-              
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
@@ -159,30 +169,26 @@ export default function Home( {pokemonData} ) {
             </Center>
 
             <Center>
-              <Text color={"black"}>Weight: {weight}lbs.</Text>
-              <Text color={"black"} ml={5}>Height: {height}ft.</Text>
+              <Text color={textColor}>Weight: {weight}lbs.</Text>
+              <Text color={textColor} ml={5}>Height: {height}ft.</Text>
             </Center>
             
             <Center mt={5} mb={2}>
-              <Text fontSize={"2xl"} fontWeight={"bold"}>Possible Moveset:</Text>
+              <Text fontSize={"2xl"} color={textColor}>Possible Moveset:</Text>
             </Center>
 
             <SimpleGrid columns={2} spacing={6}>
               {moves.map((item, index) => (
-                <Box textAlign={"center"} bgColor={"#deff8b"} borderRadius={10} padding={5}>
-                  <Text color={"black"} key={index}>{item}</Text>
+                <Box key={index} textAlign={"center"} bgColor={compColor} borderRadius={10} padding={5}>
+                  <Text color={textColor}>{item}</Text>
                 </Box>
                 
               ))}
             </SimpleGrid>
-            
-            
-            
-            
           </ModalBody>
 
           <ModalFooter>
-            <Button bgColor='#186a31' mr={3} onClick={onClose}>
+            <Button bgColor={buttonColor} color={colorMode==="light"?"black":"white"} mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalFooter>
